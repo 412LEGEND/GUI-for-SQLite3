bedeb56990d3575f93cc2a1375841233a107c3f8
SketchySketch <xzl2628655535@163.com>
2020-08-06 09:31:11 +0800

    Shorthands update
    Ordered styling shorthands

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ output = Listbox(window, width=400)
 entry.place(x=10, y=10, width=300, height=30)
 output.place(x=10, y=40, width=780, height=400)
 
-sty = input("Customizable styling: ")
+style = input("Customizable styling: ")
 
 def l2s(lst):
     strings = []
@@ -25,21 +25,19 @@ def l2s(lst):
 
 
 def stylify(var):
-    global sty
+    global style
     res = ""
     i = 0
-    while i < len(sty):
-        if sty[i] == "$":
+    while i < len(style):
+        if style[i] == "$":
             i += 1
-            if sty[i] == "{":
+            if style[i] == "{":
                 temp = 0
-                while sty[i] != "}":
+                while style[i] != "}":
                     i += 1
-                    if sty[i].isnumeric():
-                        temp = temp * 10 + int(sty[i])
-                        i += 1
-                    elif sty[i] == "}":
-                        i += 1
+                    if style[i].isnumeric():
+                        temp = temp * 10 + int(style[i])
+                    elif style[i] == "}":
                         break
                     else:
                         raise ValueError("Non-digit between '{' and '}'")
@@ -47,40 +45,67 @@ def stylify(var):
                 i += 1
             else:
                 res += "$"
-        elif sty[i] == "\\":
+        elif style[i] == "\\":
             pass
         else:
-            res += str(sty[i])
+            res += str(style[i])
             i += 1
     return res
 
 
 def stylemethods(ls):
-    global sty
-    if sty == "separation_comma":
+    global style
+    if style == "separation_comma":
         return str(ls)
-    elif sty == "separation_space":
+    elif style == "separation_space":
         temp = ""
         for elem in ls:
             temp += str(elem)
             temp += " "
         return temp
-    elif sty == "separation_semi":
+    elif style == "separation_semi":
         temp = ""
         for elem in ls:
             temp += str(elem)
             temp += "; "
         return temp
-    elif sty[1] == "\\":
-        sep = sty[1:]
+    elif style[1] == "\\":
+        sep = style[1:]
         temp = ""
         for elem in ls:
             temp += str(elem)
             temp += sep
         return temp
+    elif style == "order_num":
+        temp = ""
+        for i in range(0, len(ls)):
+            temp += str(i) + ": " + str(ls[i]) + "; "
+        return temp
+    elif style == "order_hex":
+        temp = ""
+        for i in range(0, len(ls)):
+            temp += str(hex(i)) + ": " + str(ls[i]) + "; "
+        return temp
+    elif style == "order_alnum":
+        temp = ""
+        alnum = "abcdefghijklmnopqrstuvwxyz"
+        for i in range(0, len(ls)):
+            temp += alnum[i % len(alnum)] + ": " + str(ls[i]) + "; "
+        return temp
+    elif style == "order_alnum":
+        temp = ""
+        alnum = "0123456789abcdefghijklmnopqrstuvwxyz"
+        for i in range(0, len(ls)):
+            temp += alnum[i % len(alnum)] + ": " + str(ls[i]) + "; "
+        return temp
+    elif style[0] == ":":
+        temp = ""
+        orders = style[1:].split()
+        for i in range(0, len(ls)):
+            temp += orders[i % len(orders)] + ": " + str(ls[i]) + "; "
+        return temp
     else:
-        return stylify(ls)
-    
+        return _stylify(style, ls)
 
 
 def execute():
